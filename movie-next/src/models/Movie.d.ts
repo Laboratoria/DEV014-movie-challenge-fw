b4ba9b95558f790b598d6194d0a878a5bb469866
viveth17 <95vizcarra@gmail.com>
2024-05-27 10:03:56 -0700

    feat: Define TypeScript type Movie for movie representation

---
+++ movie-next/src/models/Movie.d.ts
@@ -0,0 +1,14 @@
+
+type Movie = {
+    title: string;          // Título de la película
+    poster: string;         // URL del póster de la película
+    releaseYear: number;    // Año de lanzamiento de la película
+    director: string;      // Director de la película
+    genre: string;        // Géneros de la película 
+    duration: number;       // Duración de la película en minutos
+    rating: number;         // Calificación de la película 
+
+  };
+  
+  export default Movie;
+  
