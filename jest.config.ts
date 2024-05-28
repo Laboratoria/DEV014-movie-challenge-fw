0b09c279c1556a6226a41b3dec2545ef321a746a
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-05-28 11:03:38 -0400

    installation of dependencies and libraries: jest, babel, jsdom

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',  // Cambiar a 'jsdom'
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    
+    transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            tsconfig: 'tsconfig.json'
+          }
+        ]
+      },
+      testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+      transformIgnorePatterns: ['<rootDir>/node_modules/'],
+      moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js'
+      },
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+    
+  };
