3c6c4a929e7133a4ecef41e868d0dff2e9736228
deniisolo <deniisolo@outlook.es>
2024-05-28 20:15:06 -0500

    feat: add  the  last component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,13 +1,15 @@
 import { Movie } from "../models/Movie";
 import "./MovieCard.css"
+import { useNavigate } from 'react-router-dom';
 type moviecardprops = {
  movie:Movie
 }
 
 export function MovieCard({movie}: moviecardprops) {
+  const navigate = useNavigate();
   return (
     <div className="movieClassContainer">
-      <img  src={"https://image.tmdb.org/t/p/w500/"+movie.poster}></img>
+      <img onClick={()=>{navigate(`/movie/${movie.id}`)}}  src={"https://image.tmdb.org/t/p/w500/"+movie.poster}></img>
       <h3 className="tittleName">{movie.title}</h3>
       <p className="pName">{movie.year}</p>
       <p className="pGenre">{movie.genres}</p>
--- src/models/Movie.d.ts
+++ src/models/Movie.d.ts
@@ -1,6 +1,7 @@
 // Definir un tipo TypeScript llamado Movie dentro del archivo Movie.d.ts. Incluir campos como título, póster, año de lanzamiento y cualquier otra información 
 //relevante necesaria para la interfaz de usuario y la lógica de negocio.
 export type Movie = {
+    id: number;
     title: string;
     poster: string;
     year: string;
